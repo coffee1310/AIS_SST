87b8e3693559bb4b8b82da3de22e40b957d0268c
coffee13102006 <coffee13102006@yandex.ru>
2026-05-12 19:50:41 +0000

    fix: Почита сектора, а именно множество координаторов Author: Антон

--- src/main/java/org/example/ais_sst/mapper/SectorMapper.java
+++ src/main/java/org/example/ais_sst/mapper/SectorMapper.java
@@ -13,16 +13,9 @@ import org.mapstruct.Named;
 @Mapper(componentModel = "spring")
 public interface SectorMapper {
 
+    // Убираем ignore для полей, которых нет в DTO
+    // Оставляем только те, которые действительно есть
     @Mapping(target = "photo", expression = "java(getPhotoAsBase64(entity.getPathToPhoto(), sectorPhotoService))")
-    @Mapping(target = "coordinatorId", ignore = true)
-    @Mapping(target = "coordinatorFullName", ignore = true)
-    @Mapping(target = "coordinatorName", ignore = true)
-    @Mapping(target = "coordinatorSurname", ignore = true)
-    @Mapping(target = "coordinatorPatronymic", ignore = true)
-    @Mapping(target = "coordinatorPhoto", ignore = true)
-    @Mapping(target = "coordinatorCourseNumber", ignore = true)
-    @Mapping(target = "coordinatorGroupTitle", ignore = true)
-    @Mapping(target = "coordinatorSpecialityTitle", ignore = true)
     SectorDTO toSectorDTO(Sector entity, @Context SectorPhotoService sectorPhotoService);
 
     default String getPhotoAsBase64(String photoPath, SectorPhotoService sectorPhotoService) {
